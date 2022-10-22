2770132a40d6a1ae62536a5e92e19f88d8eacfc9
Kavikumar M <kavikumarceo@gmail.com>
2022-10-22 17:26:24 +0530

    nav

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Hero.css"
+
+function Hero() {
+  return (
+    <div className="hero">
+        
+    </div>
+  )
+}
+
+export default Hero
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,8 +3,17 @@ import "./Navbar.css"
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="navbar">
+      <h1>Logo</h1>
+      <div className="links">
+        <a className='link'>Home</a>
+        <a className='link'>Github</a>
+        <a className='link'>About Us</a>
+        <a className='link'>Contact Us</a>
+        <button className='signin'>Sign In</button>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar
